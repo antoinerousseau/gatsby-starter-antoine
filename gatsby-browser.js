2206ae7b30ac28fd29e43a59743b9e68da9b0e27
Antoine Rousseau <antoine@rousseau.im>
2020-04-26 17:21:49 +0200

    onRenderBody + package infos

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,5 +4,5 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-exports.wrapPageElement = require("./src/gatsby/wrapPageElement").default
 exports.onClientEntry = require("./src/gatsby/onClientEntry").default
+exports.wrapPageElement = require("./src/gatsby/wrapPageElement").default
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,5 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
+exports.onRenderBody = require("./src/gatsby/onRenderBody").default
 exports.wrapPageElement = require("./src/gatsby/wrapPageElement").default
