d0e8deb844eaed2fc84695345ba9dc92d46ac8ae
Antoine Rousseau <antoine@rousseau.im>
2020-09-03 14:23:42 +0200

    TS 4

--- src/types/gatsby.d.ts
+++ src/types/gatsby.d.ts
@@ -1,8 +1,8 @@
 // convenience type:
-type GatsbyPage<DataType = unknown, PageContextType = unknown, StateType = unknown> = React.FC<
+declare type GatsbyPage<DataType = unknown, PageContextType = unknown, StateType = unknown> = React.FC<
   import("gatsby").PageProps<DataType, PageContextType, StateType | null>
 >
 
-type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
+declare type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
 // https://github.com/gatsbyjs/gatsby/issues/16682#issuecomment-523352028
-type LinkProps<S = unknown> = Omit<import("gatsby").GatsbyLinkProps<S>, "ref"> & { exact?: boolean }
+declare type LinkProps<S = unknown> = Omit<import("gatsby").GatsbyLinkProps<S>, "ref"> & { exact?: boolean }
