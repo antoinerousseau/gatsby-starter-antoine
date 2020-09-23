9f0d1a9de21822f29ef30a60539a2b19cba49cb7
Antoine Rousseau <antoine@rousseau.im>
2020-09-23 16:14:29 +0200

    gatsby types

--- src/gatsby/onRenderBody.tsx
+++ src/gatsby/onRenderBody.tsx
@@ -2,9 +2,8 @@ import type { GatsbySSR } from "gatsby"
 
 // import React from "react"
 
-const onRenderBody: GatsbySSR["onRenderBody"] = () => {
+const onRenderBody: GatsbySSR["onRenderBody"] = async () => {
   // https://www.gatsbyjs.org/docs/ssr-apis/#onRenderBody
-  return null // https://github.com/gatsbyjs/gatsby/issues/24229
 }
 
 export default onRenderBody
--- src/types/gatsby.d.ts
+++ src/types/gatsby.d.ts
@@ -1,6 +1,6 @@
-// convenience type:
+// convenience type + https://github.com/gatsbyjs/gatsby/pull/26974
 declare type GatsbyPage<DataType = unknown, PageContextType = unknown, StateType = unknown> = React.FC<
-  import("gatsby").PageProps<DataType, PageContextType, StateType | null>
+  import("gatsby").PageProps<DataType, PageContextType, StateType | null> & { params: Record<string, string> }
 >
 
 declare type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
