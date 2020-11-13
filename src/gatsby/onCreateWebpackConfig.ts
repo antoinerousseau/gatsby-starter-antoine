98026cda2fc211d675d9676b59eebac7eef95578
Antoine Rousseau <antoine@rousseau.im>
2020-11-13 16:35:47 +0100

    use react fast refresh instead of hot reloader

--- src/gatsby/onCreateWebpackConfig.ts
+++ src/gatsby/onCreateWebpackConfig.ts
@@ -2,15 +2,11 @@ import type { GatsbyNode } from "gatsby"
 
 import path from "path"
 
-const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({ stage, actions }) => {
+const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({ actions }) => {
   // https://www.gatsbyjs.com/docs/node-apis/#onCreateWebpackConfig
   const alias = {
     // to avoid relative imports (must match `paths` in `tsconfig.json`):
     src: path.resolve(__dirname, ".."),
-  }
-  if (stage.startsWith("develop")) {
-    // hot reloading:
-    alias["react-dom"] = "@hot-loader/react-dom"
   }
   actions.setWebpackConfig({
     resolve: {
