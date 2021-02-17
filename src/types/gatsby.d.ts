34cfb1cf1e87c41cf01a94e627f981545d8fff88
Antoine Rousseau <antoine@rousseau.im>
2021-02-17 20:18:17 +0100

    deps update

--- src/types/gatsby.d.ts
+++ src/types/gatsby.d.ts
@@ -3,6 +3,5 @@ declare type GatsbyPage<DataType = unknown, PageContextType = unknown, StateType
   import("gatsby").PageProps<DataType, PageContextType, StateType | null> & { params: Record<string, string> }
 >
 
-declare type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
 // https://github.com/gatsbyjs/gatsby/issues/16682#issuecomment-523352028
 declare type LinkProps<S = unknown> = Omit<import("gatsby").GatsbyLinkProps<S>, "ref"> & { exact?: boolean }
